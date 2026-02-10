fecb8e9b31763cdd927ca142862c9223b7adcae8
Corentin <corentin.maurer@ecal.ch>
2026-02-10 16:42:20 +0100

    canne a pecbe ofnction disfnj asifiafs

---
+++ Assets/RopeVIsual.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class RopeVisual : MonoBehaviour
+{
+    public Transform startPoint; // player / gun / hand
+    public Transform endPoint;   // hook
+
+    private LineRenderer line;
+
+    void Awake()
+    {
+        line = GetComponent<LineRenderer>();
+        line.positionCount = 2;
+    }
+
+    void LateUpdate()
+    {
+        if (startPoint == null || endPoint == null)
+            return;
+
+        line.SetPosition(0, startPoint.position);
+        line.SetPosition(1, endPoint.position);
+    }
+}
